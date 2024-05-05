165cbcfad0cf0444c27e7b76c13457b50f4884e0
nasyaarnm <nasyaarnm@student.telkomuniversity.ac.id>
2024-05-05 23:15:45 +0700

    Aljabar Libraries

---
+++ Aljabar/StringLibrary/Aljabar.cs
@@ -0,0 +1,43 @@
+﻿namespace AljabarLibrary
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant < 0)
+            {
+                return new double[0];
+            }
+            else if (discriminant == 0)
+            {
+                double x = -b / (2 * a);
+                return new double[] { x };
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                return new double[] { x1, x2 };
+            }
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double akarA = a * a;
+            double akarB = b * b;
+
+            double middle = 2 * a * b;
+
+            return new double[] { akarA, middle, akarB };
+        }
+    }
+}
---
+++ tpmodul11_1302223148/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using AljabarLibrary;
+
+namespace tpmodul11_1302223148
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] persamaan1 = { 1, -3, -10 };
+            double[] akar1 = Aljabar.AkarPersamaanKuadrat(persamaan1); 
+            Console.WriteLine("Akar persamaan kuadrat:");
+            Console.WriteLine("Output: {" + string.Join(", ", akar1) + "}"); 
+
+            double[] persamaan2 = { 2, -3 };
+            double[] hasilKuadrat = Aljabar.HasilKuadrat(persamaan2);
+            Console.WriteLine("\nHasil kuadrat dari persamaan berpangkat 1:");
+            Console.WriteLine("Output: {" + string.Join(", ", hasilKuadrat) + "}"); 
+        }
+ 
+    }
+
+}
